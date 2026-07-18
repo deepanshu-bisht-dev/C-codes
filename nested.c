c1803e59ceb1820d5723e6039f00e1a63fcebaab
Deepanshu Bisht <deepanshu180607@gmail.com>
2026-07-18 21:38:55 +0530

    Refactor output messages in nested.c

--- nested.c
+++ nested.c
@@ -3,15 +3,15 @@
 int main() {
      int num = 10;
     if (num > 0) {
-        printf("Number is positive.\n");
+        printf("The number is positive.\n");
         if (num % 2 == 0) {
-            printf("Number is even.\n");
+            printf("The number is even.\n");
         } else {
-            printf("Number is odd.\n");
+            printf("The number is odd.\n");
         }
 
     } else {
-        printf("Number is non-positive.\n");
+        printf("Number is not positive.\n");
     }
 
     return 0;
